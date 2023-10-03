12cdc40be1842fb7328f32c70cece176e9ed4fa1
Danilo <danilonogueiraleite@gmail.com>
2023-10-03 00:16:19 -0300

    commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import Header from './components/Cabecalho'
-import Hero from './components/Hero'
-import ListaVagas from './containers/ListaVagas'
+import React from 'react';
+import styled from 'styled-components';
+import Header from './components/Cabecalho';
+import Hero from './components/Hero';
+import ListaVagas from './containers/ListaVagas';
+import GlobalStyles from './GlobalStyles';
 
-import './global.css'
+const Container = styled.div
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+  padding-bottom: 120px;
+
+  @media (max-width: 1024px) {
+    max-width: 80%;
+  }
+;
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
